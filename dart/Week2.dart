42926d1a1e87781eb67533f4b797a9bf680a64b2
Supakit <6302041520155@email.kmutnb.ac.th>
2021-08-05 17:06:45 +0700

    function + comment

--- dart/Week2.dart
+++ dart/Week2.dart
@@ -3,7 +3,7 @@ void main(){
 
    var x = callColor("white");
    print(x);
-
+//send google and https to protocol variable and 443 to port variable
 callweb("google",protocol: "https",port: 443);
 
 
@@ -12,16 +12,19 @@ callweb("google",protocol: "https",port: 443);
 
 int callColor([String newColor = "pink"]){
 
+  // dart's array
   List<dynamic> color = ["red","blue","green"];
 
   color.add(newColor);
 
+// foreach loop
   for(var item in color){
     print(item);
   }
+  // return function length of function color
   return color.length;
  }
-
+// function callweb primary and 2 secondary / arrow with print data in function
  void callweb(String web ,{int port = 80 ,String protocol = "http"}) => print("$protocol $web : $port");
 
  
