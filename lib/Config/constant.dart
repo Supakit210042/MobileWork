35953f918493c3d8429dd2f482f431e15ac0faa1
Supakit <6302041520155@email.kmutnb.ac.th>
2021-09-16 16:43:05 +0700

    Form

--- lib/Config/constant.dart
+++ lib/Config/constant.dart
@@ -5,3 +5,5 @@ const primaryColor = Color(0xffCCFFFF);
 const SColor = Color(0xffFF0033);
 
 const TColor = Color(0xffFFFFFF);
+
+const boxsize = 20.0;
--- lib/Screen/Register.dart
+++ lib/Screen/Register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobileappweek1/Config/constant.dart';
 
 class Register extends StatefulWidget {
   const Register({ Key? key }) : super(key: key);
@@ -8,13 +9,158 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+
+var name ,surname , email , password ;
+final formkey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return SafeArea(
+      child: Scaffold(
       appBar: AppBar(
       title: Text("Register page")
       ),
-      body:  Text("Hello Register"),
-      );
+         body:  Form(
+           key: formkey,
+        child: SingleChildScrollView(
+          child: Column(
+               
+            children: [
+              
+               Text("Name",style: TextStyle(fontSize: 30,),),
+              txtname(),
+              txtsurname(),
+              txtemail(),
+              txtPassword(),
+              btnSubmit(),
+            ],
+          ),
+          
+          ),
+        ),
+        ),
+        );
+           
+          
   }
+  Widget txtname(){
+    return Container(
+      margin: EdgeInsets.all(30),
+      child: TextFormField(
+                  style: TextStyle(color: primaryColor,
+                  fontSize: 24,
+                  ),
+                   decoration: InputDecoration(
+                  labelText: 'Name',
+                  icon:Icon(Icons.account_circle),
+                    hintText: "Please input Your name",
+                   ), 
+                   validator: (value) {
+                     if(value!.isEmpty){
+                       return 'Please fill your Intel in this site.';
+                     }else if (value.length < 2){
+                        return 'Please fill your Intel more than 2 Letter.';
+                     }
+                   },
+                   onSaved: (value){
+                     name = value;
+                   },
+                  ),
+    );
+  }
+    Widget txtsurname(){
+    return Container(
+      margin: EdgeInsets.all(30),
+      child: TextFormField(
+                  style: TextStyle(color: primaryColor,
+                  fontSize: 24,
+                  ),
+                   decoration: InputDecoration(
+                  labelText: 'Surname',
+                  icon:Icon(Icons.add_reaction_sharp),
+                    hintText: "Please input Your Surname",
+                   ), 
+                   validator:  (value){
+                     if (value!.isEmpty){
+                       return 'Please fill your Intel in this site.';
+                     }else if (value.length < 2){
+                        return 'Please fill your Intel more than 2 Letter.';
+                     }
+                   },
+                     onSaved: (value){
+                     surname = value!.trim();
+                   },
+                  ),
+    );
+}
+ Widget txtemail(){
+    return Container(
+      margin: EdgeInsets.all(30),
+      
+      child: TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(color: primaryColor,
+                  fontSize: 24,
+                  ),
+                   decoration: InputDecoration(
+                  labelText: 'Email',
+                  icon:Icon(Icons.add_to_photos),
+                    hintText: "Please input Your Email",
+                   ), 
+                   validator:  (value){
+                     if (!(value!.contains('@'))){
+                       return 'Please fill @ in your Email.';
+
+                    }else if (!(value.contains('.'))){
+                       return 'Please fill . in your Email';
+                    }
+                   },
+                     onSaved: (value){
+                     email = value;
+                   },
+                  ),
+    );
+}
+Widget txtPassword(){
+    return Container(
+      margin: EdgeInsets.all(30),
+      child: TextFormField(
+            obscureText: true,
+                  style: TextStyle(
+                  fontSize: 24,
+                  ),
+                   decoration: InputDecoration(
+                  labelText: 'Password',
+                  icon:Icon(Icons.add_ic_call_rounded),
+                    hintText: "Please input Your Password",
+                   ), 
+                   validator:  (value){
+                     if (value!.isEmpty){
+                       return 'Please fill your Intel in this site.';
+                     }else if (value.length < 2){
+                        return 'Please fill your Intel more than 2 Letter.';
+                     }
+                   }
+                   ,
+                     onSaved: (value){
+                     password = value;
+                   },
+                  ),
+    );
+}
+Widget btnSubmit() =>  ElevatedButton(
+  style: ElevatedButton.styleFrom(
+    //shape: Size(60, 60),
+    ),
+  onPressed: (){
+  
+  print("HEllo");
+  if(formkey.currentState!.validate()){
+    formkey.currentState!.save();
+    print("name : $name ,$surname , $email ,$password");
+  }
+  
+},
+
+child: Text('Submit'));
 }
